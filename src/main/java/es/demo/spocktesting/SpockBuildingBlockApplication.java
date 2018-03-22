e4501b20c53cd5a682e587759786f2b3eef9ee31
Tiberiu Carp <carp_tiby@ymail.com>
2018-03-22 10:12:18 +0100

    Initial commit: A Spring Boot starter application with the following libraries: spring-boot-starter-actuator spring-boot-starter-data-mongodb-reactive spring-boot-starter-webflux lombok spring-boot-starter-test reactor-test

---
+++ src/main/java/es/demo/spocktesting/SpockBuildingBlockApplication.java
@@ -0,0 +1,12 @@
+package es.demo.spocktesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpockBuildingBlockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpockBuildingBlockApplication.class, args);
+	}
+}
